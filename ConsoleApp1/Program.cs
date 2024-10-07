34937fe8f327b6f21001236c201e5df07c56e894
Olesya Lenartovich <olesyalenartovich@gmail.com>
2024-10-07 16:47:58 +0300

    function renamed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
 Console.OutputEncoding = Encoding.UTF8;
-double factorial_WhileLoop(int number)
+double FactorialFunction(int number)
 {
     double result = 1;
     while (number != 1)
@@ -17,5 +17,5 @@ Console.Write("введите число: ");
 double x = Convert.ToDouble(Console.ReadLine());
 
 
-double y = x - (Math.Pow(x,3)/factorial_WhileLoop(3))  + (Math.Pow(x,5)/ factorial_WhileLoop(5)) - (Math.Pow(x, 7) / factorial_WhileLoop(7)) + (Math.Pow(x, 9) / factorial_WhileLoop(9)) - (Math.Pow(x, 11) / factorial_WhileLoop(11)) + (Math.Pow(x, 13) / factorial_WhileLoop(13));
+double y = x - (Math.Pow(x,3)/FactorialFunction(3))  + (Math.Pow(x,5)/ FactorialFunction(5)) - (Math.Pow(x, 7) / FactorialFunction(7)) + (Math.Pow(x, 9) / FactorialFunction(9)) - (Math.Pow(x, 11) / FactorialFunction(11)) + (Math.Pow(x, 13) / FactorialFunction(13));
 Console.WriteLine(y);
